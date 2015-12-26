1117b7941ecded15852da437e46e6667a1656b6f
rjbernsen <rjbdevel@gmail.com>
2015-12-26 15:10:26 -0600

    Update forms to be consistent in style

--- quinnrose/static/quinnrose/js/forms.js
+++ quinnrose/static/quinnrose/js/forms.js
@@ -1,38 +1,10 @@
 
-
-
-$('select').change(function() {
- if ($(this).children('option:first-child').is(':selected')) {
-   $(this).addClass('select-placeholder');
- } else {
-  $(this).removeClass('select-placeholder');
- }
+$(document).ready( function() {
+	$('select').change(function() {
+		 if ($(this).children('option:first-child').is(':selected')) {
+			 $(this).addClass('select-placeholder');
+		 } else {
+			 $(this).removeClass('select-placeholder');
+		 }
+	});
 });
-
-
-
-
-
-// ----------------------------------------------
-// Sign in form
-//----------------------------------------------
-//$(function() {
-//
-//    $('#signin-form-link').click(function(e) {
-//		$("#signin-form").delay(100).fadeIn(100);
-// 		$("#register-form").fadeOut(100);
-//		$('#register-form-link').removeClass('active');
-//		$(this).addClass('active');
-//		e.preventDefault();
-//	});
-//	$('#register-form-link').click(function(e) {
-//		$("#register-form").delay(100).fadeIn(100);
-// 		$("#signin-form").fadeOut(100);
-//		$('#signin-form-link').removeClass('active');
-//		$(this).addClass('active');
-//		e.preventDefault();
-//	});
-//
-//});
-//
-//
