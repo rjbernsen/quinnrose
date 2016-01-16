c195f8388416b0a4d9582880997ff97bf482b770
rjbernsen <rjbdevel@gmail.com>
2016-01-15 19:08:45 -0600

    Re-design home page - option 3

--- quinnrose/static/quinnrose/js/base.js
+++ quinnrose/static/quinnrose/js/base.js
@@ -1,5 +1,7 @@
-DEFAULT_ALERT_CLOSE_DELAY = 5000;
-DEFAULT_ALERT_TYPE = "info";
+var DEBUG = false;
+
+var DEFAULT_ALERT_CLOSE_DELAY = 5000;
+var DEFAULT_ALERT_TYPE = "info";
 
 /*
  * All jQuery AJAX posts require this!
@@ -73,6 +75,12 @@ var spinner = $('#spinner');
 
 $(document).ready(function() {
 
+	if (DEBUG) { 
+		$(window).resize(function() {
+			showAlert($(window).width() + ' x ' + $(window).height(), 2000);
+		});
+	}
+	
 	isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
 	isAndroid = /(android)/i.test(navigator.userAgent);
 
